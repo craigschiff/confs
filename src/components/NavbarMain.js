73f203fde670412a634378d6f099575e12fd7b60
github username <meingorn@gmail.com>
2017-04-21 16:43:22 -0400

    dropdown changes

--- src/components/NavbarMain.js
+++ src/components/NavbarMain.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import Navbar from 'react-bootstrap/lib/Navbar';
 import Search from './Search'
+import LoginPage from './LoginPage'
 import Button from 'react-bootstrap/lib/Button';
 import DropdownButton from 'react-bootstrap/lib/DropdownButton';
 import ButtonGroup from 'react-bootstrap/lib/ButtonGroup';
@@ -11,52 +12,51 @@ import NavDropdown from 'react-bootstrap/lib/NavDropdown';
 
 
 class NavbarMain extends React.Component {
+  // links need to be added once route is made for topics and cities
 
   showTopics(){
-    return this.props.topics.map((topic) => {
-      return  <MenuItem><Link to={`events`} role="menuitem" >{topic.name}</Link></MenuItem>
+  const topicsArray =   ["Android", "Artificial Intelligence", "C", "C++", "C#", "Go", "Java", "JavaScript", "Microsoft", "Objective-C", "Perl", "Python", "PHP", "R", "Ruby", "Scratch", "Swift"]
+    return topicsArray.map((topic) => {
+    return <MenuItem><Link to={`events`} role="menuitem" >{topic}</Link></MenuItem>
+    })
+  }
+
+  showEvents(){
+    return this.props.events.map((event) => {
+    return <MenuItem><Link to={`events`} role="menuitem" >{event.organizer.name}</Link></MenuItem>
+    })
+  }
+
+  showCities(){
+  const citiesArray = ["New York", "San Francisco", "Chicago", "Los Angeles", "Boston", "Tel Aviv", "Seattle", "Berlin", "Singapore", "Paris", "Sao Paulo", "Moscow", "Austin", "Bangalore", "Sydney", "Toronto", "Vancouver", "Amsterdam", "Montreal"]
+    return citiesArray.map((city) => {
+    return <MenuItem><Link to={`events`} role="menuitem" >{city}</Link></MenuItem>
     })
   }
 
   render() {
-
     return(
       <div>
         <Navbar inverse collapseOnSelect>
           <Navbar.Header>
             <Navbar.Brand>
-              <a href="/events">DevConf</a>
+              <a href="/events">DevCons</a>
             </Navbar.Brand>
             <Navbar.Toggle />
           <Search />
           </Navbar.Header>
           <Navbar.Collapse>
             <NavDropdown eventKey={1} title="Or Select By Topic" id="basic-nav-dropdown-1">
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Android</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Artificial Intelligence</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >C</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >C++</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >C#</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Go</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Java</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >JavaScript</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Microsoft</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Objective-C</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Perl</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Python</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >PHP</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >R</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Ruby</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Scratch</Link></MenuItem>
-              <MenuItem eventkey={1.1}><Link to={`events`} role="menuitem" >Swift</Link></MenuItem>
+              <MenuItem>{this.showTopics()}</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey={2} title="Or By Organizer" id="basic-nav-dropdown-2">
-              <MenuItem>{this.showTopics()}</MenuItem>
+              <MenuItem>{this.showEvents()}</MenuItem>
             </NavDropdown>
             <NavDropdown eventKey={3} title="Or By City" id="basic-nav-dropdown-3">
-              <MenuItem>{this.showTopics()}</MenuItem>
+              <MenuItem>{this.showCities()}</MenuItem>
             </NavDropdown>
           </Navbar.Collapse>
+          <Link to='/login'>Login/Sign Up</Link>
         </Navbar>
       </div>
 
