fe029f512b9363eba86595f4a2137ae6f157c1cb
github username <meingorn@gmail.com>
2017-04-21 11:08:26 -0400

    truncated, formatted event component

--- src/components/ListEvent.js
+++ src/components/ListEvent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
 import Col from 'react-bootstrap/lib/Col'
-
+import Truncate from 'react-truncate';
 
 
 class ListEvent extends React.Component {
@@ -13,16 +13,38 @@ class ListEvent extends React.Component {
   // listItem() {
   //   debugger
   // }
+
   render() {
     let event = this.props.event
     return(
       <Col xs={6} md={4}>
         <Link to={`/events/${event.id}`} > {event.name} </Link><br />
-        {event.description}<br />
-        {event.date}<br />
+        <strong>Date:</strong>{event.date.split('T').shift().split('-').reverse().join('/')}<br />
         {event.cost}<br />
+        <Truncate lines={6} ellipsis={<Link to={`/events/${event.id}`}>...<br/>  Read more...</Link>}>
+           {event.description}
+        </Truncate>
+        <br />
+        {event.image}
         </Col>
     )
   }
 }
+
+
+  // function formatDate(date) {
+  //   var year = date.getFullYear(),
+  //     month = date.getMonth() + 1, // months are zero indexed
+  //     day = date.getDate(),
+  //     hour = date.getHours(),
+  //     minute = date.getMinutes(),
+  //     second = date.getSeconds(),
+  //     hourFormatted = hour % 12 || 12, // hour returned in 24 hour format
+  //     minuteFormatted = minute < 10 ? "0" + minute : minute,
+  //     morning = hour < 12 ? "am" : "pm";
+  //
+  // return month + "/" + day + "/" + year + " " + hourFormatted + ":" +
+  //         minuteFormatted + morning;
+  // }
+
 export default ListEvent
